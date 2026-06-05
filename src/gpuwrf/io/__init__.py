c74e2612e5a070768a6bf3d97be65a7f0f52b86a
nric <enric.r.g@gmail.com>
2026-06-05 08:55:10 +0100

    v0110 restart netcdf and wrfout KI-3 coverage

--- src/gpuwrf/io/__init__.py
+++ src/gpuwrf/io/__init__.py
@@ -9,6 +9,14 @@ from gpuwrf.io.wrfout_writer import (
     WRFOUT_VARIABLE_SPECS,
     write_wrfout_netcdf,
 )
+from gpuwrf.io.wrfrst_netcdf import (
+    WRF_STANDARD_RESTART_VARIABLES,
+    inspect_wrfrst_schema,
+    read_wrfrst_carry,
+    read_wrfrst_state,
+    write_wrfrst_carry,
+    write_wrfrst_state,
+)
 
 __all__ = [
     "DOWNSTREAM_CRITICAL_VARIABLES",
@@ -18,10 +26,16 @@ __all__ = [
     "MINIMUM_WRFOUT_VARIABLES",
     "OPERATIONAL_WRFOUT_VARIABLES",
     "WRFOUT_VARIABLE_SPECS",
+    "WRF_STANDARD_RESTART_VARIABLES",
     "domain_mask",
+    "inspect_wrfrst_schema",
     "lead_time_slice",
     "load_gen2_var",
+    "read_wrfrst_carry",
+    "read_wrfrst_state",
     "regrid",
     "unit_convert",
     "write_wrfout_netcdf",
+    "write_wrfrst_carry",
+    "write_wrfrst_state",
 ]
